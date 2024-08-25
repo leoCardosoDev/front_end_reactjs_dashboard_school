f6c459b302e97c95497ca6705dc92635b01ecb69
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-24 20:06:30 -0700

    chore: add name

--- src/app/dashboard/list/teachers/page.tsx
+++ src/app/dashboard/list/teachers/page.tsx
@@ -86,9 +86,9 @@ const TeacherListPage = () => {
             </button>
           </Link>
           {role === "admin" && (
-            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-            //   <Image src="/delete.png" alt="" width={16} height={16} />
-            // </button>
+            //<button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+            // <Image src="/delete.png" alt="" width={16} height={16} />
+            //</button>
             <FormModal table="teacher" type="delete" id={item.id}/>
           )}
         </div>
@@ -111,10 +111,12 @@ const TeacherListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              //   <Image src="/plus.png" alt="" width={14} height={14} />
-              // </button>
-              <FormModal table="teacher" type="create"/>
+              <>
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                  <Image src="/plus.png" alt="" width={14} height={14} />
+                </button>
+                <FormModal table="teacher" type="create"/>
+              </>
             )}
           </div>
         </div>
